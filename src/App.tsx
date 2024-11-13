47b3d935a6d2a94c3712c3246997ceef221c893b
unknown <rizqisanjaya003@gmail.com>
2024-11-13 14:30:50 +0700

    update 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import WebcamDemo from './components/WebcamDemo';
-import ImageDemo from './components/ImageDemo';
+// import ImageDemo from './components/ImageDemo';
 
 const App = (): JSX.Element => (
   <div style={{ display: 'flex', flexDirection: 'column' }}>
